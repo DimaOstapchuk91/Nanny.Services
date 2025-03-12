70e8c44b255ceeb86abe74aac265ddbfe06a4993
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2025-03-12 16:35:19 +0200

    add firebase settings

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,6 @@ import './styles/theme.css';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
